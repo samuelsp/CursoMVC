59be378cb65213284e4a8f90e2199e01cb69054b
Samuel Silva Pereira Da Cunha <samuelspcunha@gmail.com>
2020-11-18 19:53:53 -0300

    Projeto MVC DIO - Avanade

---
+++ CursoMVC/Models/Produto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoMVC.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        [Display(Name = "Descrição")]
+        public string Descricao { get; set; }
+        [Range(1, 10, ErrorMessage = "Valor fora da faixa permitida [min: 1 - max: 10]")]
+        public int Quantidade { get; set; }
+
+        public int CategoriaId { get; set; }
+
+        public Categoria Categoria { get; set; }
+    }
+}
